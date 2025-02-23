65053da70feb423f2d6792f753e3cb5b064e0160
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-22 22:54:05 -0600

    Update ITwitterClientV2.java
    End Delete Close

--- src/main/java/io/github/redouane59/twitter/ITwitterClientV2.java
+++ src/main/java/io/github/redouane59/twitter/ITwitterClientV2.java
@@ -1,232 +1,107 @@
-package io.github.redouane59.twitter;
+End
+Stop
+Delete
+zvoid
+Close
 
-import com.github.scribejava.core.model.Response;
-import io.github.redouane59.twitter.dto.dm.DirectMessage;
-import io.github.redouane59.twitter.dto.dm.DmParameters;
-import io.github.redouane59.twitter.dto.dm.DmParameters.DmMessage;
-import io.github.redouane59.twitter.dto.dm.PostDmResponse;
-import io.github.redouane59.twitter.dto.endpoints.AdditionalParameters;
-import io.github.redouane59.twitter.dto.list.TwitterList;
-import io.github.redouane59.twitter.dto.list.TwitterListList;
-import io.github.redouane59.twitter.dto.others.BearerToken;
-import io.github.redouane59.twitter.dto.others.BlockResponse;
-import io.github.redouane59.twitter.dto.rules.FilteredStreamRulePredicate;
-import io.github.redouane59.twitter.dto.space.Space;
-import io.github.redouane59.twitter.dto.space.SpaceList;
-import io.github.redouane59.twitter.dto.space.SpaceState;
-import io.github.redouane59.twitter.dto.stream.StreamRules.StreamMeta;
-import io.github.redouane59.twitter.dto.stream.StreamRules.StreamRule;
-import io.github.redouane59.twitter.dto.tweet.LikeResponse;
-import io.github.redouane59.twitter.dto.tweet.RetweetResponse;
-import io.github.redouane59.twitter.dto.tweet.Tweet;
-import io.github.redouane59.twitter.dto.tweet.TweetCountsList;
-import io.github.redouane59.twitter.dto.tweet.TweetList;
-import io.github.redouane59.twitter.dto.tweet.TweetParameters;
-import io.github.redouane59.twitter.dto.user.User;
-import io.github.redouane59.twitter.dto.user.UserActionResponse;
-import io.github.redouane59.twitter.dto.user.UserList;
-import java.util.List;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 
-public interface ITwitterClientV2 {
 
-  /**
-   * Retreive a user from his screen name calling https://api.twitter.com/2/users/
-   *
-   * @param userName the name of the targeted user
-   * @return an user object related to the targeted user
-   */
-  User getUserFromUserName(String userName);
 
-  /**
-   * Retreive a user from his id calling https://api.twitter.com/2/users/
-   *
-   * @param userId the id of the user
-   * @return an user object related to the targeted user
-   */
-  User getUserFromUserId(String userId);
 
-  /**
-   * Retreive a list of users from their usernames calling https://api.twitter.com/2/users/
-   *
-   * @param userNames the names of the targeted user
-   * @return an list of user objects related to the targeted users
-   */
-  List<User> getUsersFromUserNames(List<String> userNames);
 
-  /**
-   * Retreive a list of users from their ids calling https://api.twitter.com/2/users/
-   *
-   * @param userIds the id of the user
-   * @return an list of user object related to the targeted users
-   */
-  List<User> getUsersFromUserIds(List<String> userIds);
 
-  /**
-   * Get a list of the user followers limited to 1000 results calling https://api.twitter.com/2/users/:id/followers
-   *
-   * @param userId the id of the targeted user
-   * @return a list of users who are following the targeted user
-   */
-  UserList getFollowers(String userId);
 
-  /**
-   * Get a list of the user followers limited to 1000 results calling https://api.twitter.com/2/users/:id/followers
-   *
-   * @param userId the id of the targeted user
-   * @param additionalParameters accepted parameters are maxResults, pagination_token
-   * @return a list of users who are following the targeted user
-   */
-  UserList getFollowers(String userId, AdditionalParameters additionalParameters);
 
-  /**
-   * Get a list of the user following limited to 1000 results calling https://api.twitter.com/2/users/:id/following
-   *
-   * @param userId the id of the targeted user
-   * @return a list of users that the targeted user is following
-   */
-  UserList getFollowing(String userId);
 
-  /**
-   * Get a list of the user following limited to 1000 results calling https://api.twitter.com/2/users/:id/following
-   *
-   * @param additionalParameters accepted parameters are maxResults, pagination_token
-   * @param userId the id of the targeted user
-   * @return a list of users that the targeted user is following
-   */
 
-  UserList getFollowing(String userId, AdditionalParameters additionalParameters);
 
-  /**
-   * Get a tweet from its id calling https://api.twitter.com/2/tweets
-   *
-   * @param tweetId id of the tweet
-   * @return a tweet object
-   */
-  Tweet getTweet(String tweetId);
 
-  /**
-   * Get a tweet list from their id calling https://api.twitter.com/2/tweets
-   *
-   * @param tweetIds the ids of the tweets
-   * @return a tweet object list
-   */
-  TweetList getTweets(List<String> tweetIds);
 
-  /**
-   * Hide/Unide a reply using https://api.twitter.com/labs/2/tweets/:id/hidden
-   *
-   * @param tweetId id of the concerned reply
-   * @param hide true to hide the reply, false to unide it
-   * @return the hidden state
-   */
-  boolean hideReply(String tweetId, boolean hide);
 
-  /**
-   * Search tweets from last 7 days calling https://api.twitter.com/2/tweets/search
-   *
-   * @param query the search query
-   * @return a TweetList object containing a list of tweets
-   */
-  TweetList searchTweets(String query);
 
-  /**
-   * Search tweets from last 7 days calling https://api.twitter.com/2/tweets/search
-   *
-   * @param query the search query
-   * @param additionalParameters accepted parameters are recursiveCall, startTime, endTime, sinceId, untilId, maxResults
-   * @return a TweetList object containing a list of tweets and the next token if recursiveCall is set to false
-   */
-  TweetList searchTweets(String query, AdditionalParameters additionalParameters);
 
-  /**
-   * Search archived tweets calling https://api.twitter.com/2/tweets/search/all
-   *
-   * @param query the search query
-   * @return a TweetList object containing a list of tweets
-   */
-  TweetList searchAllTweets(String query);
 
-  /**
-   * Search archived tweets calling https://api.twitter.com/2/tweets/search/all
-   *
-   * @param query the search query
-   * @param additionalParameters accepted parameters are recursiveCall, startTime, endTime, sinceId, untilId, maxResults, nextToken
-   * @return a TweetList object containing a list of tweets and the next token if recursiveCall is set to false
-   */
-  TweetList searchAllTweets(String query, AdditionalParameters additionalParameters);
 
-  /**
-   * Stream using previous set up filters calling https://api.twitter.com/2/tweets/search/stream
-   */
-  Future<Response> startFilteredStream(Consumer<Tweet> tweet);
 
-  /**
-   * Stream using previous set up filters calling https://api.twitter.com/2/tweets/search/stream
-   */
-  Future<Response> startFilteredStream(IAPIEventListener listener);
 
-  /**
-   * Stream using previous set up filters calling https://api.twitter.com/2/tweets/search/stream
-   *
-   * @param backfillMinutes By passing this parameter, you can recover up to five minutes worth of data that you might have missed during a
-   * disconnection. The backfilled Tweets will automatically flow through a reconnected stream, with older Tweets generally being delivered before any
-   * newly matching Tweets. You must include a whole number between 1 and 5 as the value to this parameter.
-   */
-  Future<Response> startFilteredStream(IAPIEventListener listener, int backfillMinutes);
 
-  /**
-   * Stops the filtered stream with the result of the startFilteredStream. It'll wait a maximum of timeout before giving up and returning false.  If
-   * timeout isn't hit, it'll close the socket opened.
-   *
-   * @param response Future<Response> given by startFilteredStream
-   * @param timeout long How long to wait
-   * @param unit TimeUnit Units for timeout
-   */
-  boolean stopFilteredStream(Future<Response> response, long timeout, TimeUnit unit);
 
-  /**
-   * Stops the filtered stream with the result of the startFilteredStream. It'll close the socket opened.
-   *
-   * @param response Future<Response> given by startFilteredStream
-   */
-  boolean stopFilteredStream(Future<Response> response);
 
-  /**
-   * add a filtered stream rule calling https://api.twitter.com/2/tweets/search/stream/rules
-   *
-   * @param value the rule value
-   * @param tag the rule associated tag
-   * @return the created rules
-   */
-  StreamRule addFilteredStreamRule(String value, String tag);
 
-  /**
-   * add a filtered stream rule calling https://api.twitter.com/2/tweets/search/stream/rules
-   *
-   * @param value the rule value
-   * @param tag the rule associated tag
-   * @return the created rules
-   */
-  StreamRule addFilteredStreamRule(FilteredStreamRulePredicate value, String tag);
 
-  /**
-   * Delete a filtered stream rule calling https://api.twitter.com/2/tweets/search/stream/rules
-   *
-   * @param ruleValue the value of the rule to delete
-   * @return a StreamMeta object resuming the operation
-   */
-  StreamMeta deleteFilteredStreamRule(FilteredStreamRulePredicate ruleValue);
 
-  /**
-   * Delete a filtered stream rule calling https://api.twitter.com/2/tweets/search/stream/rules
-   *
-   * @param ruleValue the value of the rule to delete
-   * @return a StreamMeta object resuming the operation
-   */
-  StreamMeta deleteFilteredStreamRule(String ruleValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   /**
    * Delete a filtered stream from its rule id calling https://api.twitter.com/2/tweets/search/stream/rules
@@ -341,73 +216,7 @@ public interface ITwitterClientV2 {
    * @param targetUserId The user ID of the user that you would like the id to block.
    * @return whether the user is blocking the specified user as a result of this request.
    */
-  BlockResponse blockUser(String targetUserId);
-
-  /**
-   * Unblock a given user calling https://api.twitter.com/users/:source_user_id/blocking/:target_user_id
-   *
-   * @param targetUserId The user ID of the user that you would like the authenticated user to unblock.
-   * @return Indicates whether the user is blocking the specified user as a result of this request. The returned value is false for a successful
-   * unblock request.
-   */
-  BlockResponse unblockUser(String targetUserId);
-
-  /**
-   * Returns a list of users who are blocked by the authenticated user.
-   */
-  UserList getBlockedUsers();
-
-  /**
-   * Like a tweet calling https://api.twitter.com/2/users/:id/likes
-   *
-   * @param tweetId The ID of the Tweet that you would like the user id to Like. you must pass the Access Tokens associated with the user ID when
-   * authenticating your request.
-   * @return whether the user likes the specified Tweet as a result of this request.
-   */
-  LikeResponse likeTweet(String tweetId);
-
-  /**
-   * Unlike a tweet calling https://api.twitter.com/2/users/:id/likes/:tweet_id
-   *
-   * @param tweetId The ID of the Tweet that you would like the user id to Like. you must pass the Access Tokens associated with the user ID when
-   * authenticating your request.
-   * @return whether the user likes the specified Tweet as a result of this request.
-   */
-  LikeResponse unlikeTweet(String tweetId);
-
-  /**
-   * Allows you to get information about a Tweet’s all liking users calling https://api.twitter.com/2/tweets/:id/liking_users
-   *
-   * @param tweetId ID of the Tweet to request liking users of.
-   */
-  UserList getLikingUsers(String tweetId);
-
-  /**
-   * Allows you to get information about a Tweet’s liking users calling https://api.twitter.com/2/tweets/:id/liking_users
-   *
-   * @param tweetId ID of the Tweet to request liking users of.
-   * @param maxResults the maximum results to be returned.
-   */
-  UserList getLikingUsers(String tweetId, int maxResults);
-
-  /**
-   * Allows you to get information about a user’s liked Tweets calling https://api.twitter.com/2/users/:id/liked_tweets
-   *
-   * @param userId ID of the user to request liked Tweets for.
-   * @return a TweetList object containing a list of tweets and the next token
-   */
-  TweetList getLikedTweets(String userId);
-
-  /**
-   * Allows you to get information about a user’s liked Tweets calling https://api.twitter.com/2/users/:id/liked_tweets
-   *
-   * @param userId ID of the user to request liked Tweets for.
-   * @param additionalParameters accepted parameters are recursiveCall, maxResults, paginationToken
-   * @return a TweetList object containing a list of tweets and the next token
-   */
-  TweetList getLikedTweets(String userId, AdditionalParameters additionalParameters);
-
-
+  BlockResponse blockUser(
   /**
    * The recent Tweet counts endpoint returns count of Tweets from the last seven days that match a search query calling
    * https://api.twitter.com/2/tweets/counts/recent
