c270e24558ec50302ac3294e2ebb6d60abe09ff9
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-05 23:38:13 -0600

    Update UserV1Test.java
    End delete

--- src/test/java/io/github/redouane59/twitter/unit/UserV1Test.java
+++ src/test/java/io/github/redouane59/twitter/unit/UserV1Test.java
@@ -1,21 +1,77 @@
-package io.github.redouane59.twitter.unit;
+end
+void
+delete
+stop
+termination 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import io.github.redouane59.twitter.dto.user.UserV1;
-import io.github.redouane59.twitter.dto.user.UserV2;
-import io.github.redouane59.twitter.dto.user.UserV2.UserData;
-import org.junit.jupiter.api.Test;
 
-public class UserV1Test {
 
-  @Test
-  public void testHashCode() {
-    UserV1 user  = UserV1.builder().id("12345").build();
-    UserV1 user2 = UserV1.builder().id("23456").build();
-    assertNotEquals(user.hashCode(), user2.hashCode());
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   @Test
   public void testEquals() {
