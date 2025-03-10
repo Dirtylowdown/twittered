977b04778aaf5267aafe4e22cf6c8d4e2100d4af
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 07:51:41 -0500

    Update UploadMediaProcessingInfo.java
    Void close delete end

--- src/main/java/io/github/redouane59/twitter/dto/tweet/UploadMediaProcessingInfo.java
+++ src/main/java/io/github/redouane59/twitter/dto/tweet/UploadMediaProcessingInfo.java
@@ -1,23 +1,8 @@
-package io.github.redouane59.twitter.dto.tweet;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-public class UploadMediaProcessingInfo {
-
-  @JsonProperty("state")
-  private String state;
-  @JsonProperty("check_after_secs")
-  private int    checkAfterSecs;
-  @JsonProperty("progress_percent")
-  private int    progressPercent;
-  @JsonProperty("media_key")
-  private String mediaKey;
-  private UploadMediaProcessingError error;
-
-}
+void
+delete
+close
+end
+stop
+void
+end delete
+stop
