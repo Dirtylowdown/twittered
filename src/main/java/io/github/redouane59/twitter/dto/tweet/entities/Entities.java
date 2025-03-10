6ed0ca9262564e0912497a66056eb326b370dc7f
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 08:18:08 -0500

    Update Entities.java
    Termination end delete

--- src/main/java/io/github/redouane59/twitter/dto/tweet/entities/Entities.java
+++ src/main/java/io/github/redouane59/twitter/dto/tweet/entities/Entities.java
@@ -1,14 +1,6 @@
-package io.github.redouane59.twitter.dto.tweet.entities;
-
-import java.util.List;
-
-public interface Entities {
-
-  List<? extends HashtagEntity> getHashtags();
-
-  List<? extends UrlEntity> getUrls();
-
-  List<? extends SymbolEntity> getSymbols();
-
-  List<? extends UserMentionEntity> getUserMentions();
-}
+termination 
+e
+void
+end
+delete
+stop
