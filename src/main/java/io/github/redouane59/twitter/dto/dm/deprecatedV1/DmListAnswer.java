fc510fe2e6277beff3810b5dea88c62f810f8951
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 08:38:11 -0500

    DmListAnswer.java
    Termination void close end

--- src/main/java/io/github/redouane59/twitter/dto/dm/deprecatedV1/DmListAnswer.java
+++ src/main/java/io/github/redouane59/twitter/dto/dm/deprecatedV1/DmListAnswer.java
@@ -1,22 +1,8 @@
-package io.github.redouane59.twitter.dto.dm.deprecatedV1;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
-import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-public class DmListAnswer {
-
-  @JsonProperty("next_cursor")
-  private String              nextCursor;
-  @JsonProperty("events")
-  private List<DirectMessage> directMessages;
-  private JsonNode            apps;
-
-
-}
+termination 
+void
+end
+delete
+stop
+delete
+void
+close 
