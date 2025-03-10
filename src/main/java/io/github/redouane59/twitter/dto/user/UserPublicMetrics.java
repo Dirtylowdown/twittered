4c09ae915c27227c8779d465864e9a42f3d2a3a5
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 07:37:41 -0500

    Update UserPublicMetrics.java
    Void stop delete

--- src/main/java/io/github/redouane59/twitter/dto/user/UserPublicMetrics.java
+++ src/main/java/io/github/redouane59/twitter/dto/user/UserPublicMetrics.java
@@ -1,25 +1,7 @@
-package io.github.redouane59.twitter.dto.user;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class UserPublicMetrics {
-
-  @JsonProperty("followers_count")
-  private int followersCount;
-  @JsonProperty("following_count")
-  private int followingCount;
-  @JsonProperty("tweet_count")
-  private int tweetCount;
-  @JsonProperty("listed_count")
-  private int listedCount;
-}
+void
+end
+delete
+close
+stop
+termination
+void 
