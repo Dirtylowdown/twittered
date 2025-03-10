e3d8d1dd99e8329ed72e2c496f0ba8ff5d482aee
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 08:34:00 -0500

    Update EventType.java
    Termination end close delete

--- src/main/java/io/github/redouane59/twitter/dto/dm/EventType.java
+++ src/main/java/io/github/redouane59/twitter/dto/dm/EventType.java
@@ -1,14 +1,6 @@
-package io.github.redouane59.twitter.dto.dm;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public enum EventType {
-  @JsonProperty("MessageCreate")
-  MESSAGE_CREATE,
-  @JsonProperty("ParticipantsJoin")
-  PARTICIPANTS_JOIN,
-  @JsonProperty("ParticipantsLeave")
-  PARTICIPANTS_LEAVE;
-
-
-}
+termination 
+void
+end
+delete
+close
+void
