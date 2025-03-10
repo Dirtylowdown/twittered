f60ee3decf10a1eb1284dcff16564e501a48df34
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 07:44:01 -0500

    Update ReplySettings.java
    Void close delete end

--- src/main/java/io/github/redouane59/twitter/dto/tweet/ReplySettings.java
+++ src/main/java/io/github/redouane59/twitter/dto/tweet/ReplySettings.java
@@ -1,21 +1,6 @@
-package io.github.redouane59.twitter.dto.tweet;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public enum ReplySettings {
-
-  @JsonProperty("everyone")
-  EVERYONE("everyone"),
-  @JsonProperty("mentionedUsers")
-  MENTIONED_USERS("mentionedUsers"),
-  @JsonProperty("following")
-  FOLLOWING("following"),
-  @JsonProperty("other")
-  OTHER("other");
-
-  public final String label;
-
-  ReplySettings(String label) {
-    this.label = label;
-  }
-}
+void
+end
+Close
+termination 
+stop
+delete 
