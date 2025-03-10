038c2258ee2c7a097a44ee6c6c0c366ee4c544fd
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 08:33:17 -0500

    Update DirectMessage.java
    close delete stop end

--- src/main/java/io/github/redouane59/twitter/dto/dm/DirectMessage.java
+++ src/main/java/io/github/redouane59/twitter/dto/dm/DirectMessage.java
@@ -1,15 +1,8 @@
-package io.github.redouane59.twitter.dto.dm;
+void
+termination 
+end
+delete
+close
+stop
+void
 
-import io.github.redouane59.twitter.dto.tweet.TweetV2.Includes;
-import io.github.redouane59.twitter.dto.user.UserList.UserMeta;
-import java.util.List;
-import lombok.Getter;
-
-@Getter
-public class DirectMessage {
-
-  List<DmEvent> data;
-
-  Includes includes;
-  UserMeta meta;
-}
