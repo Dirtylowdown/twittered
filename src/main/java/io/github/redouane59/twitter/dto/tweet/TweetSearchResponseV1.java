a3823be76a74ec51af9917d288396a468100206d
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 07:49:01 -0500

    Update TweetSearchResponseV1.java
    Void stop end

--- src/main/java/io/github/redouane59/twitter/dto/tweet/TweetSearchResponseV1.java
+++ src/main/java/io/github/redouane59/twitter/dto/tweet/TweetSearchResponseV1.java
@@ -1,15 +1,6 @@
-package io.github.redouane59.twitter.dto.tweet;
-
-import java.util.List;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-public class TweetSearchResponseV1 {
-
-  private List<TweetV1> results;
-  private String        next;
-}
+void 
+close
+end
+delete
+stop
+void
