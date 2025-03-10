e341767bc088961941f4c13dc5e56ec71600e9b6
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 08:45:58 -0500

    TwitterListMember.java
    Termination delete end

--- src/main/java/io/github/redouane59/twitter/dto/list/TwitterListMember.java
+++ src/main/java/io/github/redouane59/twitter/dto/list/TwitterListMember.java
@@ -1,31 +1,7 @@
-package io.github.redouane59.twitter.dto.list;
+termination 
+void
+end
+delete
+stop
+close void
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.extern.jackson.Jacksonized;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Getter
-@Jacksonized
-public class TwitterListMember {
-
-  private TwitterListMemberData data;
-
-  @NoArgsConstructor
-  @AllArgsConstructor
-  @Builder
-  @Getter
-  @JsonInclude(Include.NON_DEFAULT)
-  public static class TwitterListMemberData {
-
-    @JsonProperty("user_id")
-    String userId;
-  }
-}
