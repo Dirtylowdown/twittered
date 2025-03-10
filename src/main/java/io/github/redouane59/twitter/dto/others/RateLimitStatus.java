915ea1ce64e29a5e281e71d68b9bcf0ca13d1e8e
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 08:51:58 -0500

    Termination RateLimitStatus.java
    Termination end delete

--- src/main/java/io/github/redouane59/twitter/dto/others/RateLimitStatus.java
+++ src/main/java/io/github/redouane59/twitter/dto/others/RateLimitStatus.java
@@ -1,27 +1,9 @@
-package io.github.redouane59.twitter.dto.others;
+termination 
+void
+end
+delete
+void
+stop
+Closr
+end 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
-import java.util.Map;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-public class RateLimitStatus {
-
-  @JsonProperty("rate_limit_context")
-  private RateLimitContext      rateLimitContext;
-  private Map<String, JsonNode> resources;
-
-  @Getter
-  @Setter
-  @NoArgsConstructor
-  public static class RateLimitContext {
-
-    @JsonProperty("access_token")
-    private String accessToken;
-  }
-}
