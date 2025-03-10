307f3ac697cb213143d15bd102e059f863a9e6c3
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 08:21:12 -0500

    Update Attachments.java
    Termination end void

--- src/main/java/io/github/redouane59/twitter/dto/tweet/Attachments.java
+++ src/main/java/io/github/redouane59/twitter/dto/tweet/Attachments.java
@@ -1,22 +1,7 @@
-package io.github.redouane59.twitter.dto.tweet;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.extern.jackson.Jacksonized;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Jacksonized
-public class Attachments {
-
-  @JsonProperty("media_keys")
-  private String[] mediaKeys;
-
-}
+termination 
+void
+end
+delete
+void
+close
+stop
