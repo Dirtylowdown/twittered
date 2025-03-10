5259ce8c879234bd0c3012d6960fca69b3065c39
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 08:40:43 -0500

    DmParameters.javate
    Termination end stop delete

--- src/main/java/io/github/redouane59/twitter/dto/dm/DmParameters.java
+++ src/main/java/io/github/redouane59/twitter/dto/dm/DmParameters.java
@@ -1,29 +1,8 @@
-package io.github.redouane59.twitter.dto.dm;
+termination 
+void
+end
+delete 
+stop
+void
+close
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.extern.jackson.Jacksonized;
-
-@Builder
-@Jacksonized
-public class DmParameters {
-
-  @JsonProperty("conversation_type")
-  @Builder.Default
-  private String       conversationType = "Group";
-  @JsonProperty("message")
-  private DmMessage    message;
-  @JsonProperty("participant_ids")
-  private List<String> participantIds;
-
-  @Builder
-  @Getter
-  @Jacksonized
-  public static class DmMessage {
-
-    private String text;
-    private String attachments; // @to be improved
-  }
-}
