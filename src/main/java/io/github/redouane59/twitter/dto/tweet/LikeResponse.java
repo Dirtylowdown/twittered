02a1f291db9ec7d6ab56df189e172aa6709ec267
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 08:11:41 -0500

    Update LikeResponse.java
    Void Ebd delete

--- src/main/java/io/github/redouane59/twitter/dto/tweet/LikeResponse.java
+++ src/main/java/io/github/redouane59/twitter/dto/tweet/LikeResponse.java
@@ -1,30 +1,7 @@
-package io.github.redouane59.twitter.dto.tweet;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.extern.jackson.Jacksonized;
-
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Jacksonized
-public class LikeResponse {
-
-  DataLikeResponse data;
-
-  @Getter
-  @Setter
-  @Builder
-  @AllArgsConstructor
-  @NoArgsConstructor
-  @Jacksonized
-  public static class DataLikeResponse {
-
-    private boolean liked;
-  }
-}
+void
+stop
+delete
+close
+end
+delete
+void
