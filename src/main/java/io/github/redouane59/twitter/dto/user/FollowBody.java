38af6b733d86f6dacba4214977c1f3bd2e2afc6a
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 07:42:51 -0500

    Update FollowBody.java
    Void stop end delete

--- src/main/java/io/github/redouane59/twitter/dto/user/FollowBody.java
+++ src/main/java/io/github/redouane59/twitter/dto/user/FollowBody.java
@@ -1,12 +1,8 @@
-package io.github.redouane59.twitter.dto.user;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class FollowBody {
-
-  @JsonProperty("target_user_id")
-  private final String targetUserId;
-
-}
+void
+delete
+end
+close
+stop
+delete
+void
+end
