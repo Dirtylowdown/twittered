e7deff1dd310ff85ae5d6f4dcd7659c756a658b1
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 08:22:41 -0500

    Update MediaCategory.java
    Termination end delete stop close

--- src/main/java/io/github/redouane59/twitter/dto/tweet/MediaCategory.java
+++ src/main/java/io/github/redouane59/twitter/dto/tweet/MediaCategory.java
@@ -1,16 +1,9 @@
-package io.github.redouane59.twitter.dto.tweet;
-
-public enum MediaCategory {
-
-  AMPLIFY_VIDEO("amplify_video"),
-  TWEET_GIF("tweet_gif"),
-  TWEET_IMAGE("tweet_image"),
-  TWEET_VIDEO("tweet_video");
-
-  public final String label;
-
-  MediaCategory(String label) {
-    this.label = label;
-  }
-
-}
+termination 
+void
+end
+stop
+Close
+void 
+void
+delete
+end 
