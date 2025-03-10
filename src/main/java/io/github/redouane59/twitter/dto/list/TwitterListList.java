2b2f0e7ce89aa5cbf8b8515bc326812d5e13083c
Dirtylowdown <cactusjacknf@gmail.com>
2025-03-10 08:47:06 -0500

    TwitterListList.java
    Termination end void

--- src/main/java/io/github/redouane59/twitter/dto/list/TwitterListList.java
+++ src/main/java/io/github/redouane59/twitter/dto/list/TwitterListList.java
@@ -1,22 +1,8 @@
-package io.github.redouane59.twitter.dto.list;
+termination 
+void
+end
+delete close void
+close
+end 
+void
 
-import io.github.redouane59.twitter.dto.list.TwitterList.TwitterListData;
-import io.github.redouane59.twitter.dto.tweet.TweetList.TweetMeta;
-import io.github.redouane59.twitter.dto.tweet.TweetV2.Includes;
-import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Getter
-public class TwitterListList {
-
-  private List<TwitterListData> data;
-  private Includes              includes;
-  private TweetMeta             meta;
-
-}
